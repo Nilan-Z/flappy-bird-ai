db6e6189d8fe66a92302522f74a40971e5074ae7
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-05 23:07:36 +0200

    Add Bird class with sprite initialization and drawing method

--- flappybird/bird.py
+++ flappybird/bird.py
@@ -1,8 +1,24 @@
+import pygame
+
 class Bird():
     def __init__(self):
         # Initialize bird properties
+        self.sprites = [
+            pygame.image.load("assets/sprites/redbird-upflap.png").convert_alpha(),
+            pygame.image.load("assets/sprites/redbird-midflap.png").convert_alpha(),
+            pygame.image.load("assets/sprites/redbird-downflap.png").convert_alpha()
+        ]
+        self.current_sprite = 0  # index du sprite courant pour l’animation
         self.x = 50
         self.y = 250
         self.gravity = 0.5
         self.jump_strength = -10
         self.velocity = 0
+        self.frame = 0
+
+    def draw(self, surface):
+        self.frame += 1
+        surface.blit(self.sprites[self.current_sprite], (self.x, self.y))
+        if self.frame == 6:
+            self.frame = 0
+            self.current_sprite = (self.current_sprite + 1) % len(self.sprites)
