b8c67a86481e2eb9c838417c831e7f23408af662
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-05 22:45:40 +0200

    Add Bird class with initialization of properties

---
+++ flappybird/bird.py
@@ -0,0 +1,8 @@
+class Bird():
+    def __init__(self):
+        # Initialize bird properties
+        self.x = 50
+        self.y = 250
+        self.gravity = 0.5
+        self.jump_strength = -10
+        self.velocity = 0
