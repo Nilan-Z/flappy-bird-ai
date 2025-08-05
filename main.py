8e9c02f7e10ee49eb27c21934c76c53d9c70b394
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-05 22:45:34 +0200

    Fix Game initialization in main.py to pass window parameter and reorder setup

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from flappybird.game import Game
 
 if __name__ == "__main__":
     pygame.init()
-    game = Game()
-    clock = pygame.time.Clock()
 
     width = 288     
     height = 512    #Original dimensions of Flappy Bird
-    fenetre = pygame.display.set_mode((width, height))
+    window = pygame.display.set_mode((width, height))
+
+    game = Game(window)
+    clock = pygame.time.Clock()
+
+
     pygame.display.set_caption("flappy-bird-ai")
 
 
