adb5729ffdbaaaaa042c1e9563c221ae5a313e98
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-05 23:07:31 +0200

    Reorganize main.py to initialize Pygame and set up game window outside of the main loop

--- main.py
+++ main.py
@@ -2,28 +2,24 @@ import pygame
 import sys
 from flappybird.game import Game
 
+pygame.init()
+
+width = 288     
+height = 512    #Original dimensions of Flappy Bird
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("flappy-bird-ai")
+
+game = Game(window)
+clock = pygame.time.Clock()
+
+
 if __name__ == "__main__":
-    pygame.init()
-
-    width = 288     
-    height = 512    #Original dimensions of Flappy Bird
-    window = pygame.display.set_mode((width, height))
-
-    game = Game(window)
-    clock = pygame.time.Clock()
-
-
-    pygame.display.set_caption("flappy-bird-ai")
-
-
-
     while True:
-        #Future game logic will go here
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-
+        game.update()
         pygame.display.flip()
         clock.tick(60)
