a640d0449d930a37ee123b96414f591e02e86028
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-06 19:13:59 +0200

    Refactor Game class to implement game over logic and collision detection

--- flappybird/game.py
+++ flappybird/game.py
@@ -2,7 +2,7 @@ import pygame
 from flappybird.bird import Bird
 from flappybird.pipe import Pipe
 
-class Game():
+class Game:
     def __init__(self, surface):
         self.surface = surface
         self.bird = Bird()
@@ -18,11 +18,19 @@ class Game():
         self.screen_width = surface.get_width()
         self.screen_height = surface.get_height()
 
+        self.game_over_sprite = pygame.image.load("assets/sprites/gameover.png").convert_alpha()
+        self.game_over = False
+
     def spawn_pipe(self):
         self.pipes.append(Pipe(x=self.screen_width))
 
     def update(self):
         self.draw_background()
+
+        if self.game_over:
+            self.draw_game_over()
+            return
+
         self.bird.draw(self.surface)
 
         self.spawn_timer += 1
@@ -44,5 +52,28 @@ class Game():
         self.surface.blit(self.base, (self.base_x, base_y))
         self.surface.blit(self.base, (self.base_x + self.base_width, base_y))
 
+        if self.check_collision():
+            self.game_over = True
+            self.bird.velocity = 0
+
     def draw_background(self):
         self.surface.blit(self.background, (0, 0))
+
+    def draw_game_over(self):
+        x = (self.screen_width - self.game_over_sprite.get_width()) // 2
+        y = (self.screen_height - self.game_over_sprite.get_height()) // 2
+        self.surface.blit(self.game_over_sprite, (x, y))
+
+    def check_collision(self):
+        bird_rect = self.bird.get_rect()
+        base_y = self.screen_height - self.base.get_height() / 1.7
+
+        if bird_rect.bottom >= base_y:
+            return True
+
+        for pipe in self.pipes:
+            top_rect, bottom_rect = pipe.get_rects()
+            if bird_rect.colliderect(top_rect) or bird_rect.colliderect(bottom_rect):
+                return True
+
+        return False
