28d5576eb15778885b771eb406f73f3a25888fc5
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-06 18:55:55 +0200

    Add Pipe class for pipe generation and rendering in the game

---
+++ flappybird/pipe.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+
+class Pipe():
+    def __init__(self, x):
+        self.pipe_image = pygame.image.load("assets/sprites/pipe-green.png").convert_alpha()
+        self.flipped_pipe = pygame.transform.flip(self.pipe_image, False, True)
+        self.gap = 120
+        self.velocity = 3
+        self.x = x
+        self.set_height()
+
+    def set_height(self):
+        height = random.randint(50, 300)
+        self.top_y = height - self.pipe_image.get_height()
+        self.bottom_y = height + self.gap
+
+    def update(self):
+        self.x -= self.velocity
+
+    def draw(self, surface):
+        surface.blit(self.flipped_pipe, (self.x, self.top_y))
+        surface.blit(self.pipe_image, (self.x, self.bottom_y))
