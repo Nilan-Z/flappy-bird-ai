0f0f0dab05edbc5607f204a6a308f0f57ecd743d
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-13 00:19:06 +0200

    Add simple flappybird env

--- ai/flappybird_env.py
+++ ai/flappybird_env.py
@@ -1,29 +1,35 @@
 import pygame
 from flappybird.game import Game
+from main import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class FlappyBirdEnv:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((288, 512))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Flappy Bird AI")
 
-        self.game = Game(self.screen)
+        self.game = Game(self.screen, user="ai")
         self.clock = pygame.time.Clock()
 
+        self.bird = self.game.bird
+        self.pipes = self.game.pipes
+
     def reset(self):
-        self.game = Game(self.screen)
+        self.game.reset()
+        self.game.waiting_to_start = False
+        self.bird = self.game.bird
+        self.pipes = self.game.pipes
         return self.get_state()
 
     def step(self, action):
         if action == 1:
-            self.game.bird.jump()
+            self.bird.jump()
 
         self.game.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+                return self.get_state(), -1, True, {}
 
         pygame.display.flip()
         self.clock.tick(30)
@@ -35,19 +41,21 @@ class FlappyBirdEnv:
         return next_state, reward, done, {}
 
     def get_state(self):
-        bird_y = self.game.bird.y
-        bird_vel = self.game.bird.velocity
+        bird_y = self.bird.y
+        bird_vel = self.bird.velocity
+
         pipe = None
-        for p in self.game.pipes:
-            if p.x + p.width > self.game.bird.x:
+        for p in self.pipes:
+            if p.x + p.width > self.bird.x:
                 pipe = p
                 break
+
         if pipe is None:
             pipe_dist = 0
             pipe_top = 0
             pipe_bottom = 0
         else:
-            pipe_dist = pipe.x - self.game.bird.x
+            pipe_dist = pipe.x - self.bird.x
             pipe_top = pipe.top_height
             pipe_bottom = pipe.bottom_height
 
@@ -55,10 +63,9 @@ class FlappyBirdEnv:
 
     def compute_reward(self):
         if self.game.game_over:
-            return -1
+            return -1.0
         else:
             return 0.1
-        
+
     def render(self):
         pygame.display.flip()
-
